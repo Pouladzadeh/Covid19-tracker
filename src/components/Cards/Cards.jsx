2ff066cff4a19c7789ac16815528cc0c67d134ba
Parvaneh <parvaneh.pouladzadeh@gmail.com>
2020-08-25 21:12:12 -0400

    Update Cards.jsx

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
 import CardComponent from './Card/Card';
-
 import styles from './Cards.module.css';
 
 const Info = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
